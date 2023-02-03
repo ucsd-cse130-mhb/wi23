53b58bf41512d77014933513bfc5aaa4cbd1f82f
Michael Borkowski <mborkowski@ucsd.edu>
2023-02-02 16:46:47 -0800

    practice probs on data types

---
+++ src/Practice_Lec10.hs
@@ -0,0 +1,89 @@
+module Pratice_Lec10 where
+
+import Prelude hiding (map, filter, flip)
+
+-- Lecture: Wednesday, February 1, 2023
+-- Practice with Recursive Data Types
+
+-- First, we'll work with our own version of 
+--   Haskell's lists as a recursive ADT:
+
+data List = Nil           -- base constructor
+          | Cons Int List -- inductive constructor
+
+-- 1) Implement a function `map` that takes a 
+--   function f (of type Int -> Int) and a list of
+--   Ints and returns a new list that consists of
+--   `f` applied to every element of the input list.
+
+map :: (Int -> Int) -> [Int] -> [Int]
+map f xs = error "fill this in"
+
+-- >>> map negate []
+-- []
+
+-- >>> map (\n -> n * n * n) [1, 3, 5, 7]
+-- [1, 27, 125, 343]
+
+-- 2) Implement a function `filter` that takes a 
+--   function `f` (of type `Int -> Bool`) and a list of
+--   Ints and returns a new list that consists only 
+--   of those elements `x` such that `f x` evaluates
+--   to True.
+
+filter :: (Int -> Bool) -> [Int] -> [Int]
+filter f xs = error "fill this in"
+
+-- >>> filter (\x -> True) []
+-- []
+
+-- >>> filter (\x -> False) [1, 2, 3, 4, 5, 6]
+-- []
+
+-- >>> filter (\n -> n `mod` 2 == 0) [1, 2, 3, 4, 5, 6, 7]
+-- [2, 4, 6]
+
+-- Use the first definition of `Tree` from the lecture
+--   for the following two questions:
+
+data Tree = Leaf | Node Int Tree Tree
+
+-- 3) Implement a function `flipTree`, that takes a Tree
+--   as input and returns the "mirror image" of the Tree,
+--   i.e. the tree with all the left and right children
+--   swapped.
+
+flipTree :: Tree -> Tree
+flipTree t = error "fill this in"
+
+-- >>> flipTree Leaf
+-- Leaf
+
+-- >>> flipTree (Node 5 Leaf (Node 1 Leaf Leaf))
+-- Node 5 (Node 1 Leaf Leaf) Leaf
+
+-- >>> flipTree (Node 1 
+--          (Node 2 (Node 3 Leaf Leaf) Leaf) 
+--          (Node 4 Leaf Leaf) )
+-- Node 1 (Node 4 Leaf Leaf)
+--        (Node 2 Leaf (Node 3 Leaf Leaf))
+
+-- 4) Finally, implement a function `isFlipped` that takes
+--   two trees `t1` and `t2` as inputs and returns True
+--   if and only if `t1` and `t2` are the mirror images 
+--   of each other (and contain the same data).
+
+isFlipped :: Tree -> Tree -> Bool
+isFlipped t1 t2 = error "fill this in"
+
+-- >>> isFlipped Leaf Leaf
+-- True
+
+-- >>> isFlipped (Node 5 Leaf (Node 1 Leaf Leaf)) (Node 5 Leaf (Node 1 Leaf Leaf))
+-- False
+
+-- >>> isFlipped (Node 1 Leaf (Node 1 Leaf Leaf)) (Node 2 (Node 2 Leaf Leaf) Leaf)
+-- False
+
+-- >>> isFlipped (Node 1 Leaf (Node 1 Leaf Leaf)) (Node 1 (Node 1 Leaf Leaf) Leaf)
+-- True
--- src/Practice_Lec9.hs
+++ src/Practice_Lec9.hs
@@ -1,5 +1,6 @@
-module Hw3 where
+module Pratice_Lec9 where
 
+-- Lecture: Monday, January 30, 2023
 -- Practice with Product/Sum Data Types
 
 data Date = Date Int Int Int
@@ -32,8 +33,59 @@ timeFormat (Time h m s) = error "fill this in"
 
 -- 2) Write a function `incrDate` that takes a Date
 --   object as its input and returns the next date.
+--   To reduce the corner cases, just assume the date
+--   is a valid date in the 21st century.
 
-
+incrDate :: Date -> Date
+incrDate (Date m d y) = error "fill this in"
 
 -- >>> incrDate (Date 2 28 2020)
 -- Date 2 29 2020
+
+-- >>> incrDate (Date 12 31 2022)
+-- Date 1 1 2023
+
+-- 3) Recall the Paragraph data type from lecture,
+--   which combines both sum types and product types
+--   and was used to represent a toy model of a 
+--   subset of Markdown.
+
+data Paragraph = 
+    Text String          -- 3 constructors,
+  | Heading Int String   -- each with different  
+  | List Bool [String]   -- parameters
+
+--   Write a function `wordCount` which returns the total
+--   word count in a list of paragraph objects. The following
+--   function from the Prelude will be useful:
+--
+--   words :: String -> [String]
+--   "words breaks a string up into a list of words, which were 
+--   delimited by white space.""
+--
+--   >>> words "Lorem ipsum\ndolor"
+--   ["Lorem","ipsum","dolor"]"
+
+wordCount :: [Paragraph] -> Int
+wordCount ps = error "use patterns and fill this in"
+  where 
+    listWordCount :: [String] -> Int
+    listWordCount []       = error "try this helper function"
+    listWordCount (s:strs) = error "try this helper function"
+
+    paraWordCount :: Paragraph -> Int
+    paraWordCount p = error "and this one too" 
+
+-- >>> listWordCount ["wow","","this is a bullet point"]
+-- 6
+
+-- >>> paraWordCount (Heading 2 "Chapter One")
+-- 2
+
+-- >>> wordCount [Heading 2 "Chapter One",
+--                Text "These are nearby beaches",
+--                List False ["Ocean Beach", "Pacific Beach",
+--                            "Cardiff State Beach", 
+--                            "San Elijo State Beach"] ]
+-- 17
+ 
