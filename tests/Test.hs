9daea3b17b93ea5d6e713332a47d44d0414e3482
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2023-02-03 10:16:42 -0800

    feat: add Lec7 practice tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,2 +1,32 @@
+import Practice_Lec7
+import Practice_Lec8
+import Practice_Lec9
+import Practice_Lec10
+import Test.Hspec
+
 main :: IO ()
-main = print "hello world"
+main = hspec $ do
+  describe "Lecture 7: From λ to Haskell" $ do
+      describe "and3" $ do
+        specify "True True False => False" $ do
+          and3 True True False `shouldBe` False
+
+      describe "xor3" $ do
+        specify "True True False => False" $ do
+          xor3 True True False `shouldBe` False
+
+        specify "True True True => True" $ do
+          xor3 True True True `shouldBe` True
+
+        specify "False True False => False" $ do
+          xor3 False True False `shouldBe` False
+
+      describe "factorial" $ do
+        specify "factorial 3 == 6" $ do
+          factorial 3 `shouldBe` 6
+
+        specify "factorial 0 == 0" $ do
+          factorial 0 `shouldBe` 0
+
+        specify "factorial (-90) == 0" $ do
+          factorial (-90) `shouldBe` 0
